964529448ad32ddf9d36c529e07648cca0019f2a
Chengjin Liu <chengjin@keyst.one>
2026-05-26 11:18:32 +0800

    feat: add blog schema data

--- src/app/[post]/page.tsx
+++ src/app/[post]/page.tsx
@@ -4,10 +4,46 @@ import { postConverter } from './utils'
 import { fetchAPI, getPostsAll } from '@/utils/api'
 import Content from './Content'
 import { Metadata, ResolvingMetadata } from 'next'
+import { Post } from '@/types/postDetailPageType'
+import Script from 'next/script'
 
 // 强制动态渲染，禁用缓存
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
+
+const formatISODate = (dateString: string): string => {
+  const date = new Date(dateString)
+  return date.toISOString().replace('Z', '+08:00')
+}
+
+const createBlogPostingSchema = (article: Post, canonicalURL: string, summary: string) => {
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'BlogPosting',
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': canonicalURL,
+    },
+    headline: article.title,
+    description: summary || article.title,
+    image: article.hero_image.data.attributes.url,
+    author: {
+      '@type': 'Person',
+      name: 'Keystone Blog',
+    },
+    publisher: {
+      '@type': 'Organization',
+      name: 'Keystone',
+      logo: {
+        '@type': 'ImageObject',
+        url: 'https://keyst.one/assets/keystone-logo-k6ycS1NB.webp',
+      },
+    },
+    datePublished: formatISODate(article.publishedAt),
+    dateModified: formatISODate(article.updatedAt),
+    isAccessibleForFree: true,
+  }
+}
 
 export async function generateMetadata(
   { params }: { params: { post: string } },
@@ -85,12 +121,26 @@ export default async function PostDetail({
 
   const postModel = postConverter(article.attributes)
   const minutesToRead = Math.ceil(readingTime(postModel.bodyText).minutes)
+  const schema = createBlogPostingSchema(
+    article.attributes,
+    postModel.seo.canonicalURL,
+    postModel.summary
+  )
 
   return (
-    <Content
-      article={article}
-      postModel={postModel}
-      minutesToRead={minutesToRead}
-    />
+    <>
+      <Script
+        id="blog-posting-schema"
+        type="application/ld+json"
+        strategy='beforeInteractive'
+      >
+        {JSON.stringify(schema)}
+      </Script>
+      <Content
+        article={article}
+        postModel={postModel}
+        minutesToRead={minutesToRead}
+      />
+    </>
   )
 }
